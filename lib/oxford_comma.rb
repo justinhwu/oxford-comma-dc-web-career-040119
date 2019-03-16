842babedd3e2a7877db83b0cdb2986aa09603f55
Justin Wu <justinhwu95@gmail.com>
2019-03-16 01:08:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ if array.length == 1
 elsif array.length ==2
 array.join(" and ")
 else
-array[0..array.length-2].join(", ") << ", and #{array[array.length-1]}"
+array[0..array.length-2].join(", ") << ", and #{array[-1]}"
 end
 end
